7bf5e55be803c243016433ff689fd1cac54d7173
sara.santoslois <sara.santoslois@B05P213B.wirtz.local>
2024-04-29 22:03:34 +0200

    commit5

--- src/main/java/slst04e10/NewMain.java
+++ src/main/java/slst04e10/NewMain.java
@@ -1,5 +1,6 @@
 package slst04e10;
 public class NewMain {
     public static void main(String[] args) {
-        System.out.println(Restador.restar (15f,4f));
+        System.out.println(Restador.restar (16f,4f));
+
 }}
