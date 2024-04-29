a65130e6a0cdb3068bb45b35c478a97fd7eefc31
sara.santoslois <sara.santoslois@B05P213B.wirtz.local>
2024-04-29 21:29:36 +0200

    commit2

---
+++ src/main/java/slst04e10/Restador.java
@@ -0,0 +1,5 @@
+package slst04e10;
+public class Restador {
+    public static float restar (float a, float b ){
+        return a - b;
+}} 
--- src/main/java/slst04e10/Sumador.java
+++
@@ -1,5 +0,0 @@
-package slst04e10;
-public class Sumador {
-    public static float sumar (float a, float b ) {
-        return a + b;
-}}
