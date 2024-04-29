aa7e0a0271145397463aa22c4c689dc018daeb01
sara.santoslois <sara.santoslois@B05P213B.wirtz.local>
2024-04-29 20:54:34 +0200

    commit1

---
+++ src/main/java/slst04e10/NewMain.java
@@ -0,0 +1,5 @@
+package slst04e10;
+public class NewMain {
+    public static void main(String[] args) {
+        System.out.println(Sumador.sumar (15f,4f));
+}}
---
+++ src/main/java/slst04e10/Sumador.java
@@ -0,0 +1,5 @@
+package slst04e10;
+public class Sumador {
+    public static float sumar (float a, float b ) {
+        return a + b;
+}}
